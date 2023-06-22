c0e7e3f1c0da35235f54efe7976cced575d99e87
Nicholas Addison <nick@addisonbrown.com.au>
2023-06-22 14:48:07 +1000

    fix: file parser test

--- src/ts/__tests__/fileParser.test.ts
+++ src/ts/__tests__/fileParser.test.ts
@@ -6,7 +6,7 @@ describe('Parser', () => {
             const files = await getSolidityFilesFromFolderOrFile(
                 './src/contracts'
             )
-            expect(files).toHaveLength(36)
+            expect(files).toHaveLength(39)
         })
 
         test('get Solidity files from folder with no sol files', async () => {
